c22020cc01a8a352a574ddc3d2cf3777cae094a0
itskevviin <141619108+itskevviin@users.noreply.github.com>
2023-11-03 09:32:28 +0530

    Add files via upload

---
+++ assignments/30.10.2023/1464.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        priority_queue<int, vector<int>> pq;
+        for (auto it : nums) {
+            pq.push(it);
+        }
+
+        int ans = 1;
+        for (int i = 0; i < 2; i++) {
+            int temp = pq.top();
+            pq.pop();
+            cout << temp << endl;
+            ans *= (temp - 1);
+        }
+
+        return ans;
+    }
+};
